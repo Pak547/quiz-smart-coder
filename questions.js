614937d64e7eff10293ae85ed5273b49af428baa
Pak <pakapongm@gmail.com>
2023-10-16 06:14:00 -0700

    Start button prompts quiz, next button shows next question and answers, wrote logic for correct and incorrect inputs, ERROR IN EVENT CLICK LISTENER MUST FIX, after that save local storage and create a leaderboard page

--- questions.js
+++ questions.js
@@ -1,74 +1,136 @@
-const quiz = querySelector('.quizSection');
-const goNext = querySelector('.next');
+const optionList = document.querySelector('optionsList');
+const root = document.querySelector(".main");
+const question = document.querySelector(".questionText");
+const options = Array.from(document.querySelectorAll(".option"));
+const scoreText = document.querySelector(".score");
 
-let questionCount;
+let currentQuestion = {};
+let answers = true;
 let scoreCount = 0;
 let count = 5;
 let countdown;
+let availableQuestions = [];
 
 const questionArray = [
   {
-    num:"0",
     question:"CSS is an acronym for what?",
     options: [
-      "Cascade Some Source",
-      "Company Sample Style",
-      "Cascading Style Sheets",
-      "Cool Simplified Sauce",
+      "A. Cascade Some Source",
+      "B. Company Sample Style",
+      "C. Cascading Style Sheets",
+      "D. Cool Simplified Sauce",
     ],
-    correct: "Cascading Style Sheets",
+    correct: "C. Cascading Style Sheets",
   },
   {
-    num:"1",
     question:"Which is an example of a 'Void' tag?",
     options:  [
-      "<br>",
-      "<body>",
-      "<a>",
-      "<header>",
+      "A. <br>",
+      "B. <body>",
+      "C. <a>",
+      "D. <header>",
     ],
-    correct: "<br>"
+    correct: "A. <br>"
   },
   {
-    num:"2",
     question:"What does Luigi (our instructor) preach we look for if  we don't know something?",
     options: [
-      "ChatGPT",
-      "Documentation",
-      "Reddit",
-      "Jesus",
+      "A. ChatGPT",
+      "B. Documentation",
+      "C. Reddit",
+      "D.Jesus",
     ],
-    correct: "Documentation"
+    correct: "B. Documentation"
 },
 {
-  num:"3",
   question:"Where is the correct place to insert a Javascript",
   options:[
-    "The <head> section",
-    "Both the <head> section and the <body> section are correct",
-    "The <body> section",
-    "It automatically links"
+    "A. The <head> section",
+    "B. Both the <head> section and the <body> section are correct",
+    "C. The <body> section",
+    "D. It automatically links"
   ],
   correct: "Both the <head> section and the <body> section are correct"
 },
 {
-  num:"4",
   question:"The 'external' Javascript file must contain the <script> tag",
   options:[
-    "True",
-    "False"
+    "A. True",
+    "B. False"
   ],
-  correct: "False"
+  correct: "B. False"
 },
 {
-  num:"5",
   question:"How do you call a function named 'callBackFunction'?",
   options:[
-    "call function callBackFunction();",
-    "callBackFunction();",
-    "call callBackFunction();"
+    "A. call function callBackFunction();",
+    "B. callBackFunction();",
+    "C. call callBackFunction();"
   ],
-  correct:"callBackFunction();"
+  correct:"B. callBackFunction();"
 }
 ]
 
+
+
+
+/*
+const maxQuestions = 4;
+
+startBtn.onclick = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questionArray];
+  getNewQuestion();
+}
+
+getNewQuestion = () => {
+  if(availableQuestions.length === 0) {
+    localStorage.setItem('recentScore', score)
+
+    return window.location.assign('/end.html')
+  }
+
+/*
+  const questionsIndex = Math.floor(Math.random()* availableQuestions.length)
+  currentQuestion = availableQuestions[questionsIndex]
+  question.innerText = currentQuestion.question
+
+  options.forEach(option => {
+    const number = option.dataset['number'];
+    option.innerText = currentQuestion['option' +number];
+  })
+  // Removes elements from array
+  availableQuestions.splice(questionsIndex, 1)
+
+  answers = true;
+}
+
+options.forEach (option => {
+  option.addEventListener('click', e=> {
+    if(!acceptingAnswers) return
+    acceptingAnswers = false
+    const selectedChoice =e.target
+    const selectedAnswer = selectedChoice.dataset['number']
+  // using ternary op toggle the option to be either correct or incorrect answer
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+  // if its right then we add a point
+    if(classToApply ==='correct') {
+      incrementScore(scorePoints)
+    }
+    selectedChoice.parentElement.classList.add(classToApply)
+  // Gives time to transition to the next question
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getNewQuestion()
+    }, 1000)
+
+
+  })
+})
+
+incrementScore = num => {
+  score+= num 
+  scoreText.innerText = score;
+}
+*/
--- script.js
+++ script.js
@@ -17,11 +17,79 @@ const timer = document.querySelector(".timer");
 const leaderBoard = document.querySelector(".leaderBoard");
 const quiz = document.querySelector(".quizSection");
 const homePage = document.querySelector(".home");
-const content = document.querySelector(".content");
-const root = document.querySelector(".main");
+const quizContainer = document.querySelector('.quizContainer')
+
+const question = document.querySelector(".questionText");
+const options = document.querySelectorAll(".option");
+const scoreText = document.querySelector(".score");
+const nextBtn = document.querySelector(".next");
+const putQuestionsHere = document.querySelector(".emptyDiv");
 
 let secondsLeft = 80;
+let questionCount = 0;
+let answers = true;
+let userScore = 0;
+let countdown;
 
+var questionArray = [
+  {
+    num: 1,
+    question:"CSS is an acronym for what?",
+    options:[ 
+      "A. Cascade Some Source",
+      "B. Company Sample Style",
+      "C. Cascading Style Sheets",
+      "D. Cool Simplified Sauce"
+    ],
+    answer: "C. Cascading Style Sheets"
+  },
+  {
+    num: 2,
+    question:"What does Luigi (our instructor) preach we look for if  we don't know something?",
+    options:[
+      "A. ChatGPT",
+      "B. Documentation",
+      "C. Reddit",
+      "D. Jesus"
+    ],
+    answer: "B. Documentation"
+},
+{
+  num: 3,
+  question:"Where is the correct place to insert a Javascript",
+  options:[
+    "A. The <head> section",
+    "B. Both the <head> section and the <body> section are correct",
+    "C. The <body> section",
+    "D. It automatically links"
+  ],
+  answer: "B. Both the <head> section and the <body> section are correct"
+},
+{
+  num: 4,
+  question:"The 'external' Javascript file must contain the <script> tag",
+  options:[
+    "A. True",
+    "B. False",
+    "C. Sometimes",
+    "D. Only in Python"
+  ],
+  answer: "B. False"
+},
+{
+  num: 5,
+  question:"How do you call a function named 'callBackFunction'?",
+  options:[
+    "A. call function callBackFunction();",
+    "B. callBackFunction();",
+    "C. call callBackFunction();",
+    "D. Function(callBack);"
+  ],
+  answer:"B .callBackFunction();"
+}
+]
+
+// function for decrementing timer 
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
@@ -30,15 +98,147 @@ function setTime() {
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
-        alert("Failed");
+        return leaderBoard;
       }
     }, 1000);
 }
+
+// function to show each question and potential answers
+function showQuestions(index,) {
+  let questionCount = 0;
+  questionCount++; // Move the increment statement here
+  const questionText = document.querySelector('.questionText');
+  // changes the question number and question with each click
+  questionText.textContent = `${questionArray[index].num}. ${questionArray[index].question}`;
+  let optionTag = '';
+// makes sure every since option is the questionarray is being properly input
+  for (let i = 0; i < questionArray[index].options.length; i++) {
+    optionTag += `<div class="option"><span data-number="${i+1}">${questionArray[index].options[i]}</span></div>`;
+  }
+  putQuestionsHere.innerHTML = optionTag;
+}
+
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questionArray[questionCount].answer;
+  let allOptions = optionList.children.length;
+
+  if(userAnswer == correctAnswer) {
+    answer.classList.add('correct');
+    userScore++;
+    headerScore();
+  }
+  else {
+    answer.classList.add('wrong')
+    secondsLeft -= 5;
+
+    // if answer incorrect auto select correct answer
+    for(let i=0; i< allOptions; i++) {
+      if (optionList.children[i].textContent == correctAnswer){
+        optionList.children[i].setAttribute('class','option correct');
+      }
+    }
+  }
+  for (let i = 0; i < options.length; i++) {
+    options[i].classList.add('disabled');
+  }
+  nextBtn.classList.add('active');
+
+  // Move to the next question after a delay
+  setTimeout(() => {
+    questionCount++;
+    if (questionCount < questionArray.length) {
+      showQuestions(questionCount);
+      // Reset option styles
+      for (let i = 0; i < options.length; i++) {
+        options[i].classList.remove('correct', 'wrong', 'disabled');
+      }
+    } else {
+      // Quiz is finished, handle end of quiz logic
+      return leaderBoard;
+    }
+  }, 1000);
+}
+
+
+/*
+function optionSelected(selectedOption) {
+  // Get the selected option's data-number attribute
+  const selectedOptionNumber = selectedOption.getAttribute('data-number');
+
+  // Compare the selected option with the correct answer
+  if (selectedOptionNumber === questionArray[questionCount].answer) {
+    // The user selected the correct answer
+    // increase score if correct and add class correct
+    selectedOption.classList.add('correct');
+    scoreCount++;
+    scoreText.textContent = `Score: ${scoreCount}`;
+    console.log("correct");
+  } else {
+    // The user selected the wrong answer
+    // assign 5 second penalty
+    selectedOption.classList.add('wrong');
+    secondsLeft -= 5;
+    console.log("wrong");
+  }
+  for (let i=0; i <allOptions; i++) {
+    if (putQuestionsHere.children[i].textContent == correctAnswer){
+      putQuestionsHere.children[i].setAttribute('class', 'option correct');
+    }
+  }
+  // Disable selecting options after selection temporarily
+  for (let i = 0; i < options.length; i++) {
+    options[i].classList.add('disabled');
+  }
+
+  // Move to the next question after a delay
+  setTimeout(() => {
+    questionCount++;
+    if (questionCount < questionArray.length) {
+      showQuestions(questionCount);
+      // Reset option styles
+      for (let i = 0; i < options.length; i++) {
+        options[i].classList.remove('correct', 'wrong', 'disabled');
+      }
+    } else {
+      // Quiz is finished, handle end of quiz logic
+      return leaderBoard;
+    }
+  }, 1000);
+}
+*/
+function selectOption() {
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', function() {
+      optionSelected(this);
+    });
+  }
+}
+
+function headerScore () {
+  const headerScoreText = document.querySelector(".score");
+  headerScoreText.textContent =`Score: ${userScore}/ ${questionArray.length}`;
+}
+
 
 startBtn.onclick = () => {
   quiz.classList.add('active');
   timer.classList.add('active');
   homePage.classList.add('disabled');
-  root.classList.add('disabled');
+  quizContainer.classList.add('active');
+
   setTime();
+  showQuestions(0);
+  selectOption();
+  headerScore();
+}
+
+nextBtn.onclick = () => {
+  if (questionCount < questionArray.length) {
+    questionCount++;
+    showQuestions(questionCount);
+  } else {
+    
+  }
 }
